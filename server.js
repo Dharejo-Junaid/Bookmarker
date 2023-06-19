34562436d5ed1c8bea42ccff502f15b9185de780
Dharejo-Junaid <junaidali.100190@gmail.com>
2023-06-19 09:07:17 +0100

    Signup page

--- server.js
+++ server.js
@@ -11,22 +11,43 @@ app.get("/", (req, res) => {
     res.status(200).render("index", {});
 });
 
+app.get("/signup", (req, res) => {
+    res.status(200).render("signup", {});
+});
+
 app.post("/", async (req, res) => {
     const {email, password} = req.body;
-    
-    let emailExist = false;
 
-    const passHash = await hash(password, 10);
-    console.log(passHash);
+    // get user from database
+    let user = {email: "junaid@gmail.com",passwordHash: "$2b$10$CvUffTF92ZYJOvP5kc53nengtq0BYienvIt37UQZ8N4DCPqULAiv."};
 
-    res.redirect("/");
+    if(email === user.email) {
+
+        compare(password, user.passwordHash, (err, same) => {
+            if(same) res.send("Welcome");
+            else res.send("Wrong password");
+        });
+
+    } else {
+        res.send(`User with email: ${email} does not exist`);
+    }
+});
+
+app.post("/signup", (req, res) => {
+    const {name, email, password, confPassword} = req.body;
+    console.log(name, email, password, confPassword);
+
+    // get user from database
+    let user = {email: "junaid@gmail.com",passwordHash: "$2b$10$CvUffTF92ZYJOvP5kc53nengtq0BYienvIt37UQZ8N4DCPqULAiv."};
+
+    res.send("Account created");
 });
 
 
 
 app.listen(5000, () => {
     console.log("Server is listening at 5000");
-})
+});
 
 
 /*
@@ -40,5 +61,13 @@ app.listen(5000, () => {
             console.log(same);
         }
     });
+
+*/
+
+
+/*
+
+    const passHash = await hash(password, 10);
+    console.log(passHash);
 
 */
